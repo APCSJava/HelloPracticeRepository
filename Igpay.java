34f8adfa9ade9afda383f5a02cff85b1765292bd
Kent Collins <kent_collins@asl.org>
2014-09-26 13:13:37 +0100

    Adding completed skeleton code for Igpay.java

---
+++ Igpay.java
@@ -0,0 +1,28 @@
+/**
+ * A translator that accepts an English word and prints to the console 
+ * the equivalent word that results from transformation under the rules
+ * of Pig Latin.  Words are transformed according to one of three patterns:
+ * 
+ * for initial consonants, move the leading consonant to the end, + "ay"
+ * for initial blends, move the blend to the end and add "ay"
+ * for initial vowels, simply add "way" to the end of the word
+ * 
+ * Ex. 
+ * 		"happy" should print "appyhay"
+ * 		"trap" should print "aptray"
+ * 		"egg" should print "eggway"
+ * 
+ * @author Kent Collins
+ * @version 26 September 2014
+ */
+
+import java.util.Scanner;
+
+public class Igpay {
+	public static void main(String[] args) {
+		// TODO Prompt the user for input and assign to a variable
+		// TODO Determine whether word starts with a single consonant, vowel, or blend
+		// TODO Transform the word into a new String
+		// TODO Print the result to the console
+	}
+}
